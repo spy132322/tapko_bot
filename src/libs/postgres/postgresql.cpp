9e324a0e75c2df34204e7064d6665153f8ed22f9
Spy132 <ruslan.bitrek@gmail.com>
2025-03-05 23:18:14 +0300

    изменено:      .gitignore 	новый файл:    src/libs/postgres/ma.cpp 	изменено:      src/libs/postgres/postgresql.cpp

--- src/libs/postgres/postgresql.cpp
+++ src/libs/postgres/postgresql.cpp
@@ -1,19 +1,19 @@
-
 #include <string>
-#include "postgresql.h"
 #include <pqxx/pqxx>
 #include <iostream>
+#include "postgresql.h"
+
 namespace psql
 {
     // connect_string "dbname=your_db user=your_user password=your_password hostaddr=your_server port=5432"
     DB::DB(const std::string &connect_info)
     {
         c_info = connect_info;
-    }
+    };
 
 };
 // Verify Tables
-bool check_tables(std::string &c)
+void check_tables(std::string &c)
 {
     try
     {
