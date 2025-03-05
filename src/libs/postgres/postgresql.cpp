21cc45652dcfccfdf702b7db6d674b93989fec1e
Spy132 <ruslan.bitrek@gmail.com>
2025-03-06 00:33:14 +0300

    изменено:      src/libs/postgres/postgresql.cpp 	изменено:      src/libs/postgres/tables.cpp

--- src/libs/postgres/postgresql.cpp
+++ src/libs/postgres/postgresql.cpp
@@ -3,12 +3,14 @@
 #include <iostream>
 #include "postgresql.h"
 #include "tables.cpp"
+void check_tables(std::string &c);
 namespace psql
 {
     // connect_string "dbname=your_db user=your_user password=your_password hostaddr=your_server port=5432"
     DB::DB(const std::string &connect_info)
     {
         c_info = connect_info;
+        check_tables(c_info);
     };
 
 };
@@ -21,23 +23,36 @@ void check_tables(std::string &c)
         std::cout << "[II] Connected to DB" << std::endl;
         std::cout << "[II] Verifying DB scheme" << std::endl;
         pqxx::work tr{conn};
-        if(verify::user(tr)){
+        if (verify::user(tr))
+        {
             std::cout << "[II] User tables exists" << std::endl;
         }
-        else{
+        else
+        {
             create_tables::create_users(tr);
             std::cout << "[EE] Cannot find user table, creating new one" << std::endl;
         }
-        if(verify::wch_table(tr)){
+        if (verify::wch_table(tr))
+        {
             std::cout << "[II] User tables exists" << std::endl;
         }
-        else{
+        else
+        {
             create_tables::create_wch(tr);
             std::cout << "[EE] Cannot find watchers table, creating new one" << std::endl;
+        }
+        if (verify::dates(tr))
+        {
+            std::cout << "[II] Dates tables exists" << std::endl;
+        }
+        else
+        {
+            create_tables::create_dates(tr);
+            std::cout << "[EE] Cannot find dates table, creating new one" << std::endl;
         }
     }
     catch (std::exception &e)
     {
-        std::cout << e.what() << std::endl;
+        std::cout << "[EE] " << e.what() << std::endl;
     }
 }
--- src/libs/postgres/tables.cpp
+++ src/libs/postgres/tables.cpp
@@ -40,23 +40,61 @@ namespace verify
             std::cout << "[EE] " << e.what() << std::endl;
         }
     }
+    bool dates(pqxx::work &tr)
+    {
+        try
+        {
+            bool isExisting = tr.query_value<bool>(
+                R"(
+                SELECT EXISTS (
+                    SELECT 1
+                    FROM pg_tables
+                    WHERE schemaname = 'public' AND tablename = 'dates'
+                );
+                )");
+            return isExisting;
+        }
+        catch (std::exception &e)
+        {
+            std::cout << "[EE] " << e.what() << std::endl;
+        }
+    }
 };
-namespace create_tables{
+namespace create_tables
+{
     // Создание таблицы дежурных
-    void create_wch(pqxx::work &tr){
-        try{
+    void create_wch(pqxx::work &tr)
+    {
+        try
+        {
             tr.exec(R"(CREATE TABLE IF NOT EXISTS watchers (name1 VARCHAR(100) NOT NULL, name2 VARCHAR(100) NOT NULL, isKilled BOOLEAN, isWas BOOLEAN);)");
         }
-        catch(std::exception &e){
+        catch (std::exception &e)
+        {
             std::cout << "[EE] " << e.what() << std::endl;
         }
     }
     // Создание таблицы пользователей
-    void create_users(pqxx::work &tr){
-        try{
-            tr.exec(R"(CREATE TABLE IF NOT EXISTS users(id INTERGER NOT NULL, admin BOOLEAN, Username TEXT, Name TEXT, UNIQUE("id"));)");
+    void create_users(pqxx::work &tr)
+    {
+        try
+        {
+            tr.exec(R"(CREATE TABLE IF NOT EXISTS users(id INTERGER NOT NULL,isAutosend BOOLEAN, isAdmin BOOLEAN, Username TEXT, Name TEXT, UNIQUE("id"));)");
         }
-        catch(std::exception &e){
+        catch (std::exception &e)
+        {
+            std::cout << "[EE] " << e.what() << std::endl;
+        }
+    }
+    // Создание таблицы дат исключений
+    void create_dates(pqxx::work &tr)
+    {
+        try
+        {
+            tr.exec(R"(CREATE TABLE IF NOT EXISTS dates(date TEXT, UNIQUE("date"));)");
+        }
+        catch (std::exception &e)
+        {
             std::cout << "[EE] " << e.what() << std::endl;
         }
     }
