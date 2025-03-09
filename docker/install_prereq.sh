741ba3899d54b436ba065d55e01afcf9f24ba3a6
Spy132 <rus-lvds@mail.ru>
2025-03-09 03:55:35 +0300

    новый файл:    docker/DockerFile 	новый файл:    docker/docker-compose.yml     новый файл:    docker/install_prereq.sh

---
+++ docker/install_prereq.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+# !!! ЗАПУСКАТЬ НЕ НА ХОСТЕ !!!
+apt update
+apt upgrade -y
+apt update && apt install -y locales && \
+    locale-gen en_US.UTF-8 && \
+    locale-gen ru_RU.UTF-8 && \
+    update-locale LANG=en_US.UTF-8
+apt install -y build-essential g++ make binutils cmake libboost-system-dev libssl-dev zlib1g-dev libcurl4-openssl-dev cmake git libpq-dev postgresql-server-dev-all libboost-dev libssl-dev pkg-config libgtest-dev libgmock-dev doxygen
+cd /
+git clone https://github.com/reo7sp/tgbot-cpp
+cd /tgbot-cpp
+cmake .
+make -j4
+make install
+cd /
+git clone https://github.com/jtv/libpqxx.git /libpqxx
+cd /libpqxx
+cmake .
+cmake --build .
+cmake --install .
+cd /
+git clone https://github.com/spy132322/tapko_bot.git
+cd /tapko_bot
+cmake .
+make
+make install
+cd /
+
+
